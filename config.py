896a837d1f1c79380c71255bbb94a9703da3cd33
bharti085 <84670462+bharti085@users.noreply.github.com>
2025-06-19 08:45:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 import google.generativeai as genai
 
 # Set your Gemini API key here
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyAV8foqN_11AH8GIMYN6H7xanLTQwxkQ2Y")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
 genai.configure(api_key=GEMINI_API_KEY)
 
-os.environ["GOOGLE_API_KEY"] = "AIzaSyAV8foqN_11AH8GIMYN6H7xanLTQwxkQ2Y"
+os.environ["GOOGLE_API_KEY"] = ""
